541abeb356b6a8f471d0394c951ba282989677a2
Marsi <agalliumars@gmail.com>
2022-12-21 21:45:20 +0100

    Barista's Challenge

--- Item.java
+++ Item.java
@@ -1,5 +1,26 @@
 public class Item {
 
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- TestOrders.java
+++ TestOrders.java
@@ -1,85 +1,33 @@
 public class TestOrders {
     public static void main(String[] args) {
 
-        // Menu items
-        Item item1 = new Item();
-        item1.name = "mocha";
-        item1.price = 2.25;
+        Item item1 = new Item("mocha", 2.0);
+        Item item2 = new Item("latte", 1.5);
+        Item item3 = new Item("cappuccino", 2.5);
+        Item item4 = new Item("drip coffee", 1.0);
 
-        Item item2 = new Item();
-        item2.name = "latte";
-        item2.price = 2.0;
-
-        Item item3 = new Item();
-        item3.name = "drip coffee";
-        item3.price = 1.5;
-
-        Item item4 = new Item();
-        item4.name = "cappuccino";
-        item4.price = 2.5;
-
-        // Order variables -- order1, order2 etc.
         Order order1 = new Order();
-        order1.name = "Cindhuri";
-        order1.total = 0;
+        order1.display();
 
         Order order2 = new Order();
-        order2.name = "Jimmy";
-        order2.items.add(item1);
-        order2.total = order2.total + item1.price;
-        order2.ready = true;
+        order2.display();
 
-        Order order3 = new Order();
-        order3.name = "Noah";
-        order3.items.add(item4);
-        order3.total += item4.price;
+        Order order3 = new Order("Sam");
+        order3.addItem(item1);
+        order3.addItem(item2);
+        order3.display();
+        order3.setReady(true);
+        System.out.println(order3.getStatusMessage());
 
-        Order order4 = new Order();
-        order4.name = "Sam";
-        order4.items.add(item2);
-        order4.total += item2.price;
-        order4.items.add(item2);
-        order4.items.add(item2);
-        order4.total += item2.price;
+        Order order4 = new Order("Jimmy");
+        order4.addItem(item4);
+        order4.addItem(item1);
+        order4.display();
 
-
-        // Application Simulations
-        // Use this example code to test various orders' updates
-        System.out.printf("Name: %s\n", order1.name);
-        System.out.printf("Total: %s\n", order1.total);
-        System.out.printf("Items: %s\n", order1.items);
-        if (order1.ready && order1.total > 0.0) {
-            System.out.println("Your order is ready");
-        } else if(order1.total > 0.0){
-            System.out.println("Your order is getting ready");
-        }
-
-        System.out.printf("Name: %s\n", order2.name);
-        System.out.printf("Total: %s\n", order2.total);
-        System.out.printf("Items: %s\n", order2.items);
-        if (order2.ready && order2.total > 0.0) {
-            System.out.println("Your order is ready");
-        } else if(order2.total > 0.0){
-            System.out.println("Your order is getting ready");
-        }
-
-        System.out.printf("Name: %s\n", order3.name);
-        System.out.printf("Total: %s\n", order3.total);
-        System.out.printf("Items: %s\n", order3.items);
-        if (order3.ready && order3.total > 0.0) {
-            System.out.println("Your order is ready");
-        } else if(order3.total > 0.0){
-            System.out.println("Your order is getting ready");
-        }
-
-        System.out.printf("Name: %s\n", order4.name);
-        System.out.printf("Total: %s\n", order4.total);
-        System.out.printf("Items: %s\n", order4.items);
-        if (order4.ready && order4.total > 0.0) {
-            System.out.println("Your order is ready");
-        } else if(order4.total > 0.0){
-            System.out.println("Your order is getting ready");
-        }
-
+        Order order5 = new Order("Cindhuri");
+        order5.addItem(item2);
+        order5.addItem(item3);
+        order5.display();
     }
+
 }
